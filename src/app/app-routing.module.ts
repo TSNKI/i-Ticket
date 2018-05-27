8c80e3bc081a664ee1cffcac4dfc57b45079e6dd
TSNKI <929509707@qq.com>
2018-05-27 15:45:16 +0800

    Add app-home.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {AppHomeComponent} from './app-home/app-home.component';
 
 const routes: Routes = [
   {path: 'user/login', component: LoginComponent},
-  // { path: '', redirectTo: '/user/login', pathMatch: 'full' },
+  {path: '', component: AppHomeComponent},
+  // { path: '', redirectTo: 'user/login', pathMatch: 'full' },
 ];
 
 @NgModule({
