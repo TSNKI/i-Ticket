cf0cfbaeabc72335b9faad9e9ae9c63806c2e63a
TSNKI <929509707@qq.com>
2018-06-01 19:56:45 +0800

    Add cookies.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ngCookies} from 'angular-cookies';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
---
+++ src/app/services/cookies.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CookiesService {
+
+  constructor() {
+  }
+
+  setCookie(name: string, value: any, timeout: any): void {
+    const d = new Date();
+    d.setDate(d.getDate() + timeout);
+    document.cookie = name + '=' + value + ';expires=' + d;
+  }
+
+  getCookie(name: string): string {
+    const arr = document.cookie.split('; ');
+    for (let i = 0; i < arr.length; i++) {
+      const arr2 = arr[i].split('='); // ['abc','cba']
+      if (arr2[0] === name) {
+        console.log(arr2[1]);
+        return arr2[1];
+      }
+    }
+    return '';
+
+  }
+}
