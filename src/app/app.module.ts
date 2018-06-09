3b28b5a5017663a177417f0ab04dd498afea98d8
tsnki <929509707@qq.com>
2018-06-09 16:10:42 +0800

    Remove ngCookies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {ngCookies} from 'angular-cookies';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
