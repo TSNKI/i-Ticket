68c8926c742a33cc71d421af578aeae585fcd80e
TSNKI <929509707@qq.com>
2018-06-25 13:11:19 +0800

    Finish carousel effect
    Signed-off-by: TSNKI <929509707@qq.com>

--- src/app/vip/vip-home/vip-home.component.ts
+++ src/app/vip/vip-home/vip-home.component.ts
@@ -1,8 +1,8 @@
-import { AfterViewInit, Component, HostListener, Inject, OnInit } from '@angular/core';
-import { NzCarouselComponent } from 'ng-zorro-antd';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { DOCUMENT } from '@angular/common';
 import { EventService } from '../../services/event.service';
+import { NzCarouselComponent } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-vip-home',
@@ -23,59 +23,70 @@ import { EventService } from '../../services/event.service';
     ])
   ]
 })
-export class VipHomeComponent implements OnInit, AfterViewInit {
+export class VipHomeComponent implements OnInit {
 
   hotSpotData = [
     {
       id: 0,
-      title: '游侠索罗',
-      link: ''
+      title: '梅赛德斯-奔驰特别呈现 Mike Shinoda 麦克·信田上海演唱会',
+      link: '',
+      img: 'assets/images/1806151351072999_m1.jpg'
     },
     {
       id: 1,
-      title: '游侠索罗',
-      link: ''
+      title: '梅赛德斯-奔驰特别呈现 Mike Shinoda 麦克·信田上海演唱会',
+      link: '',
+      img: 'assets/images/1806151351072999_m1.jpg'
     },
     {
       id: 2,
-      title: '游侠索罗',
-      link: ''
+      title: '梅赛德斯-奔驰特别呈现 Mike Shinoda 麦克·信田上海演唱会',
+      link: '',
+      img: 'assets/images/1806151351072999_m1.jpg'
     },
     {
       id: 3,
-      title: '游侠索罗',
-      link: ''
+      title: '梅赛德斯-奔驰特别呈现 Mike Shinoda 麦克·信田上海演唱会',
+      link: '',
+      img: 'assets/images/1806151351072999_m1.jpg'
     },
     {
       id: 4,
-      title: '游侠索罗',
-      link: ''
+      title: '梅赛德斯-奔驰特别呈现 Mike Shinoda 麦克·信田上海演唱会',
+      link: '',
+      img: 'assets/images/1806151351072999_m1.jpg'
     },
     {
       id: 5,
-      title: '游侠索罗',
-      link: ''
+      title: '梅赛德斯-奔驰特别呈现 Mike Shinoda 麦克·信田上海演唱会',
+      link: '',
+      img: 'assets/images/1806151351072999_m1.jpg'
     },
     {
       id: 6,
-      title: '游侠索罗',
-      link: ''
+      title: '梅赛德斯-奔驰特别呈现 Mike Shinoda 麦克·信田上海演唱会',
+      link: '',
+      img: 'assets/images/1806151351072999_m1.jpg'
     },
     {
       id: 7,
-      title: '游侠索罗',
-      link: ''
+      title: '梅赛德斯-奔驰特别呈现 Mike Shinoda 麦克·信田上海演唱会',
+      link: '',
+      img: 'assets/images/1806151351072999_m1.jpg'
     },
     {
       id: 8,
-      title: '游侠索罗',
-      link: ''
+      title: '梅赛德斯-奔驰特别呈现 Mike Shinoda 麦克·信田上海演唱会',
+      link: '',
+      img: 'assets/images/1806151351072999_m1.jpg'
     }
   ];
 
   floatBarState = 'wrapped';
 
   categoryData: any[];
+
+  selected = [ true, false, false, false, false, false, false, false, false ];
 
   // position: number[];
 
@@ -99,12 +110,9 @@ export class VipHomeComponent implements OnInit, AfterViewInit {
     });
   }
 
-  ngAfterViewInit() {
-    // this.position = [];
-    // this.categoryData.forEach(category => {
-    //   const dom = this.doc.getElementById(category.name);
-    //   this.position.push(dom.getBoundingClientRect().top - 300);
-    // });
+  setSelected(index: number) {
+    this.selected = [ false, false, false, false, false, false, false, false, false ];
+    this.selected[ index ] = true;
   }
 
   @HostListener('window:scroll', [])
@@ -116,65 +124,4 @@ export class VipHomeComponent implements OnInit, AfterViewInit {
       this.floatBarState = 'wrapped';
     }
   }
-
-  // @HostListener('window:load', [])
-  // onWindowLoad() {
-  //   this.scrollToAnchor();
-  // }
-  //
-  // @HostListener('window:hashchange', [])
-  // onHashChange() {
-  //   this.scrollToAnchor();
-  // }
-  //
-  //
-  // private scrollToAnchor() {
-  //   // 获取伪锚点的id
-  //   const anchor = this.getAnchor();
-  //
-  //   // 如果不存在伪锚点,则直接结束
-  //   if (anchor.length < 1) {
-  //     return;
-  //   }
-  //
-  //   const anchorDom = this.getDom(anchor);
-  //   const anchorScrollTop = anchorDom.offsetTop;
-  //
-  //   alert(document.body.scrollTop + ' ' + anchorScrollTop);
-  //   this.animationToAnchor(document.body.scrollTop, anchorScrollTop);
-  // }
-  //
-  // /*
-  //     @function 滚动到指定位置方法
-  //     @param startNum {int} -- 开始位置
-  //     @param stopNum {int} -- 结束位置
-  //   */
-  // private animationToAnchor(startNum, stopNum) {
-  //   let nowNum = startNum + 10; // 步进为10
-  //
-  //   if (nowNum > stopNum) {
-  //     nowNum = stopNum;
-  //   }
-  //
-  //   // 缓动方法
-  //   window.requestAnimationFrame(() => {
-  //     document.body.scrollTop = nowNum; // 当前示例页面，滚动条在body，所以滚动body
-  //
-  //     // 滚动到预定位置则结束
-  //     if (nowNum === stopNum) {
-  //       return;
-  //     }
-  //
-  //     this.animationToAnchor(nowNum, stopNum); // 只要还符合缓动条件，则递归调用
-  //   });
-  // }
-  //
-  // private getAnchor() {
-  //   return window.location.hash.substring(1);
-  // }
-  //
-  // private getDom(id) {
-  //   return document.getElementById(id);
-  // }
-
 }
