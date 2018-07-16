6654634d0f549d72a7aa6426b7b6c6a3ec39152e
286674013 <286674013@qq.com>
2018-07-17 02:00:32 +0800

    single ticket 页面大致完成 ,注意事项如下: 1.不要点流程框的内容 2.有的样式改不动,我TM也很崩溃
    Signed-off-by: 286674013 <286674013@qq.com>

--- src/app/vip/single-ticket/single-ticket.component.ts
+++ src/app/vip/single-ticket/single-ticket.component.ts
@@ -10,6 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 import { FetchService } from '../../shared/fetch.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'it-single-ticket',
   templateUrl: './single-ticket.component.html',
@@ -48,7 +49,7 @@ export class SingleTicketComponent implements OnInit {
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
   fourthFormGroup: FormGroup;
-
+  tips: any[];
   constructor(
     private cookieService: CookiesService,
     private eventService: EventService,
@@ -74,6 +75,34 @@ export class SingleTicketComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.tips = [
+      { name: '演出时间', value: '2018.09.08 19:30', name2: '演出场馆', value2: '华润深圳湾体育中心＂春茧＂体育场' },
+      { name: '演出时长', value: '暂无相关信息，请以现场为准', name2: '入场时间', value2: '演出前约120分钟' },
+      {
+        name: '预售时间', value: '客户端预售时间:2018-06-25 09:58:00; 网页端预售时间:2018-06-25 09:58:00'
+        , name2: '限购', value2: '每个账号最多购买6张'
+      },
+      {
+        name: '儿童入场提示', value: '1.2米以上凭成人票入场，1.2米以下谢绝入场'
+        , name2: '禁止携带物品', value2: '由于安保和版权的原因，大多数演出、展览及比赛场所禁止携带食品、饮料、专业摄录设备、打火机等物品，请您注意现场工作人员和广播的提示，予以配合'
+      },
+      {
+        name: '付款时效提醒', value: '购票下单成功后需在15分钟内完成支付，未支付成功的订单，将在下单15分钟后系统自动取消，所选价位将自动释放后重新点亮，大家可及时刷新购票页面进行购买'
+        , name2: '缺货登记提醒', value2: '所需票价若为灰色，说明已经售完。您可以在当前页面进行缺货登记，后期如果有票会以短信形式及时通知'
+      },
+      {
+        name: '发票说明', value: '演出结束后将不支持开具发票申请，由此给您造成的不便我们深表歉意，感谢您的理解与支持！'
+        , name2: '实名制', value2: '购买时您需要输入购票人身份证信息，每笔订单对应一个证件号'
+      },
+      {
+        name: '座位类型', value: '请按门票对应位置，有序对号入座'
+        , name2: '物品寄存', value2: '无寄存处，请自行保管携带物品'
+      },
+      {
+        name: '纸质票入场', value: '本演出（展览、赛事）提供纸质门票，演出（观展、比赛）当天请携带收到的纸质门票，验票后入场（仅订单或订单截图无效）'
+        , name2: '不支持退换', value2: '票品不支持退换。如无法正常观看，还请自行处理，给您带来不便敬请谅解'
+      },
+    ];
     this.id = 5;
     this.datechoosed = 0;
     this.allticket = 0;
