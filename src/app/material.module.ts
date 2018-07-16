5aeacae7656223983df287f522216e74037fc294
chenpipin <gmdchanbb@gmail.com>
2018-07-16 10:55:01 +0800

    import mat-radio-button

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -23,6 +23,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatRadioButton } from "@angular/material";
 
 @NgModule({
   imports: [
@@ -50,6 +51,7 @@ import { MatChipsModule } from '@angular/material/chips';
     MatAutocompleteModule,
     MatSnackBarModule,
     MatChipsModule,
+    MatRadioButton,
   ],
   exports: [
     MatDividerModule,
@@ -76,6 +78,7 @@ import { MatChipsModule } from '@angular/material/chips';
     MatAutocompleteModule,
     MatSnackBarModule,
     MatChipsModule,
+    MatRadioButton,
   ]
 })
 export class MaterialModule {
